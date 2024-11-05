f5833a817cab47d807919f0c034b3ea920afceba
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-11-06 06:15:13 +0900

    refactor: Add /api prefix to all endpoints

--- src/main/kotlin/com/group/library_app/user/presentation/UserController.kt
+++ src/main/kotlin/com/group/library_app/user/presentation/UserController.kt
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/users")
 class UserController(
     private val userService: UserService,
 ) {
